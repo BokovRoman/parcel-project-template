9b914653e0d5798f6cbe2635cf5fe14c42cd17d9
BokovRoman <r.bokov1991@gmail.com>
2022-05-10 13:39:09 +0300

    completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+// рендеру разметки
+
+function createGalleryMarkup(galleryItems) {
+return galleryItems.map(({preview, original, description}) => 
+  { return `
+  <div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+  </div>
+  `
+  }).join("")
+}
+//  разметка в документ
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Галерея за допомогою плагіна SimpleLightbox (не потребує прослуховування кліків, розмітка будь яка) 
+
+let simpleGallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: "250ms" });
